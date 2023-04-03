baa8268f40cff31c073d65cacf23e7bf7fc34c1f
HermanNgwenya <hermanbngwenya@gmail.com>
2023-04-03 15:06:05 +0200

    IWA_05 Challenge

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
-FREE_WARNING = 'Free shipping only applies to single customer orders'
-BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
-NONE_SELECTED = '0'
+const FREE_WARNING = 'Free shipping only applies to single customer orders';
+const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence';
+const NONE_SELECTED = 0;
 
-if (location = RSA) { shipping === 400 && currency === 'R' }
+let location = 'RSA';
+let currency = 'R';
+let customers = 1;
+let shipping = 400;
 
-if location = NAM
-shipping = 600 
-else shipping = 800
-
-shoes = 300 * 1
-toys - 100 * 5
-hirts = 150 * 'NONE_SELECTED'
-batteries 35 * 2
-pens = 5 * 'NONE_SELECTED' 
-
-shipping = null
-currency = $
-
-if (shoes + batteries + pens + shirts > 1000 &&  ) {
-	if (location = NAM && customers < 2) {
-			if (location = RSA)
-		    shipping = 0 || calcShipping
-		}
-	}
+const shoes = 300 * 1;
+const toys = 100 * 5;
+const shirts = 150 * NONE_SELECTED;
+const batteries = 35 * 2;
+const pens = 5 * NONE_SELECTED; 
+let itemsprice = shoes + batteries + pens + shirts + toys;
+if (location ==='RSA'){
+	 shipping === 400; 
+}
+else if (location === 'NAM'){
+	shipping = 600 ;
+}
+else if (location === 'NK'){
+	console.log(BANNED_WARNING);
+}
+else {
+	shipping = 800;
 }
 
-if (shipping = 0) && (customers !=== 1) { console.log(WARNING) }
+if (itemsprice > 1000 && location === 'RSA'|| location === 'NAM' && customers===1){
+		    shipping = 0 ;
+		} 
+		else if (shipping === undefined){
+			console.log(BANNED_WARNING);
+		} 
+		else if ((shipping === 0 && customers !== 1)){
+			 console.log(FREE_WARNING);
+			 }
 
-location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
+const total = itemsprice + shipping;
 
-customers = '1'
-const location = 'RSA'
-currency = null
+console.log('Price:', currency, total );
